22f37588a30ff336a05ff76d455820ac3f4d1861
Nikolas  Haug <nikolashaug@er-10-129-148-9.wireless.umn.edu>
2018-06-28 21:32:11 -0500

    add api and handlebars display

--- app/routes/api-routes.js
+++ app/routes/api-routes.js
@@ -1,18 +1,65 @@
 var db = require("../models");
 
+var ebay = require("ebay-api");
+
 module.exports = function (app) {
 
-    app.get("/vendors", function (req, res) {
-        db.Providers.findAll({order: db.sequelize.col('price')})
-            .then(function (results) {
-                console.log(results);
-                // res.json(results);
-                var hbsObject = {
-                    vendor: results
-                };
-                res.render("providershbs", hbsObject);
-            });
+    
+    
+    // Run the request
+    app.get("/vendors", function(req, res) {
+
+        var params = {
+            keywords: ["entry doors"],
+            // add additional fields
+            outputSelector: ['PictureURLSuperSize'],
+            paginationInput: {
+              entriesPerPage: 10
+            },
+          };
+
+        ebay.xmlRequest({
+            serviceName: 'Finding',
+            opType: 'findItemsByKeywords',
+            appId: 'NikolasH-bootcamp-PRD-5c970d9ce-2c7dc5a8', // api key
+            params: params,
+            parser: ebay.parseResponseJson // (default)
+          },
+          // gets all the items together in a merged array
+          function itemsCallback(error, itemsResponse) {
+            if (error) throw error;
+            //create variable for the response array
+            var items = itemsResponse.searchResult.item;
+            //create the empty array of objects to hold the api data
+            var item = {}
+            var products = [];
+            //loop through the api call results
+            for (let i = 0; i < items.length; i++) {
+                item.name = items[i].title;
+                item.image = items[i].pictureURLSuperSize;
+                item.price = items[i].sellingStatus.currentPrice.amount.toFixed(2);
+                //push results into objects array
+                products.push(item);
+                //clear the object for each loop
+                item = {};
+            }  
+            console.log(products);
+            res.render("providershbs", {products: products});
+          }
+        );
     });
+
+    // app.get("/vendors", function (req, res) {
+    //     db.Providers.findAll({order: db.sequelize.col('price')})
+    //         .then(function (results) {
+    //             console.log(results);
+    //             // res.json(results);
+    //             var hbsObject = {
+    //                 vendor: results
+    //             };
+    //             res.render("providershbs", hbsObject);
+    //         });
+    // });
 
     app.get("/vendors/:budget", function (req, res) {
         db.Providers.findAll({
