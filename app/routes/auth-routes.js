21d0dca6400cb23962b62da23ad6a5824a5ad383
Barbey Ng <barbeyng@er-10-129-207-224.wireless.umn.edu>
2018-06-30 10:12:41 -0500

    categories completed

--- app/routes/auth-routes.js
+++ app/routes/auth-routes.js
@@ -11,20 +11,20 @@ module.exports = function(app, passport) {
 
     // POST request for /signup -- goes to the passport local-signup
     app.post('/signup', passport.authenticate('local-signup', {
-        successRedirect: '/profile',
+        successRedirect: '/categories',
         failureRedirect: '/'
     }));
 
     // POST request for /signin -- goes to the passport local-signin
     app.post('/signin', passport.authenticate('local-signin', {
-        successRedirect: '/profile',
-        failureRedirect: '/signin'
+        successRedirect: '/categories',
+        failureRedirect: '/'
     }));
 
     // Helper function to check if the user is pre-authenticated
     function isLoggedIn(req, res, next) {
         if (req.isAuthenticated())
             return next();
-        res.redirect('/signin');
+        res.redirect('/');
     }
 }
